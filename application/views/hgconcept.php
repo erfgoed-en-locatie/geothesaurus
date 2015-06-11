7d20f9c81cb5785be69027507c4c70466a8127e4
Ivo de Kler <ivo.de.kler@gmail.com>
2015-06-11 16:43:13 +0200

    added primitive sorting to hg concept page

--- application/views/hgconcept.php
+++ application/views/hgconcept.php
@@ -11,66 +11,74 @@
 
 <h3>PiT's met een geometrie</h3>
 
-<table class="table table-striped">
-<tr>
-<th style="width:25%;">naam</th>
-<th style="width:20%;">datering</th>
-<th>PiT</th>
-<th style="width:20%;">bron</th>
-</tr>
-<? foreach ($pits as $pit) { ?>
-	<? if($pit['geometryIndex']>-1){
-
-		$datering = "";
-		if(isset($pit['hasBeginning'])){
-			$datering = date("Y",strtotime($pit['hasBeginning']));
-		}
-		if(isset($pit['hasEnd'])){
-			$datering .= ' - ' . date("Y",strtotime($pit['hasEnd']));
-		}
-
-
-		 ?>
-		<tr class="pit">
-			<td><?= $pit['name'] ?></td>
-			<td><?= $datering ?></td>
-			<td><a href="<?= $this->config->item('base_url') ?>pit/<?= $pit['hgid'] ?>"><?= $pit['hgid'] ?></a></td>
-			<td><a href="<?= $this->config->item('base_url') ?>bron/<?= $pit['source'] ?>"><?= $pit['source'] ?></a></td>
+<table class="table table-striped sortable">
+	<thead>
+		<tr>
+			<th style="width:25%;">naam</th>
+			<th style="width:20%;">datering</th>
+			<th>PiT</th>
+			<th style="width:20%;">bron</th>
 		</tr>
+	</thead>
+	<tbody>
+	<? foreach ($pits as $pit) { ?>
+		<? if($pit['geometryIndex']>-1){
+
+			$datering = "";
+			if(isset($pit['hasBeginning'])){
+				$datering = date("Y",strtotime($pit['hasBeginning']));
+			}
+			if(isset($pit['hasEnd'])){
+				$datering .= ' - ' . date("Y",strtotime($pit['hasEnd']));
+			}
+
+
+			 ?>
+			<tr class="pit">
+				<td><?= $pit['name'] ?></td>
+				<td><?= $datering ?></td>
+				<td><a href="<?= $this->config->item('base_url') ?>pit/<?= $pit['hgid'] ?>"><?= $pit['hgid'] ?></a></td>
+				<td><a href="<?= $this->config->item('base_url') ?>bron/<?= $pit['source'] ?>"><?= $pit['source'] ?></a></td>
+			</tr>
+		<? } ?>
 	<? } ?>
-<? } ?>
+	</tbody>
 </table>
 
 
 
 <h3>PiT's zonder geometrie</h3>
 
-<table class="table table-striped">
-<tr>
-<th style="width:25%;">naam</th>
-<th style="width:20%;">datering</th>
-<th>PiT</th>
-<th style="width:20%;">bron</th>
-</tr>
-<? foreach ($pits as $pit) { ?>
-	<? if($pit['geometryIndex']<0){
-
-		$datering = "";
-		if(isset($pit['hasBeginning'])){
-			$datering = date("Y",strtotime($pit['hasBeginning']));
-		}
-		if(isset($pit['hasEnd'])){
-			$datering .= ' - ' . date("Y",strtotime($pit['hasEnd']));
-		}
-
-		 ?>
-		<tr class="pit">
-			<td><?= $pit['name'] ?></td>
-			<td><?= $datering ?></td>
-			<td><a href="<?= $this->config->item('base_url') ?>pit/<?= $pit['hgid'] ?>"><?= $pit['hgid'] ?></a></td>
-			<td><a href="<?= $this->config->item('base_url') ?>bron/<?= $pit['source'] ?>"><?= $pit['source'] ?></a></td>
+<table class="table table-striped sortable">
+	<thead>
+		<tr>
+			<th style="width:25%;">naam</th>
+			<th style="width:20%;">datering</th>
+		<th>PiT</th>
+		<th style="width:20%;">bron</th>
 		</tr>
+	</thead>
+	<tbody>
+	<? foreach ($pits as $pit) { ?>
+		<? if($pit['geometryIndex']<0){
+
+			$datering = "";
+			if(isset($pit['hasBeginning'])){
+				$datering = date("Y",strtotime($pit['hasBeginning']));
+			}
+			if(isset($pit['hasEnd'])){
+				$datering .= ' - ' . date("Y",strtotime($pit['hasEnd']));
+			}
+
+			 ?>
+			<tr class="pit">
+				<td><?= $pit['name'] ?></td>
+				<td><?= $datering ?></td>
+				<td><a href="<?= $this->config->item('base_url') ?>pit/<?= $pit['hgid'] ?>"><?= $pit['hgid'] ?></a></td>
+				<td><a href="<?= $this->config->item('base_url') ?>bron/<?= $pit['source'] ?>"><?= $pit['source'] ?></a></td>
+			</tr>
+		<? } ?>
 	<? } ?>
-<? } ?>
+	</tbody>
 </table>
 
