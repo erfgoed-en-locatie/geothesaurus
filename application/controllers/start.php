d9e7932e29efc7410a9f976dd28859e40f5d7612
Ivo de Kler <ivo.de.kler@gmail.com>
2015-06-16 12:08:33 +0200

    Fixes #3, changed apiUrl for start screen to be the same as search results

--- application/controllers/start.php
+++ application/controllers/start.php
@@ -10,7 +10,7 @@ class Start extends CI_Controller {
 
 	public function index(){
 
-		$apiurl = "https://api.histograph.io/search?";
+		$apiurl = "https://api.erfgeo.nl/search?";
 		$data['count'] = 0;
 		$data['results'] = array();
 		$q = "";
